f117dad9000d2b7ec00d145fbb21d85eb7ee16fa
illushun <daveystuart1@gmail.com>
2024-09-24 20:38:59 +0100

    SpaceEngine init

--- SpaceRaider/Engine/SpaceEngine.swift
+++ SpaceRaider/Engine/SpaceEngine.swift
@@ -1,5 +1,18 @@
 import SpriteKit
 
-class SpaceEngine {
+class SpaceEngine: NSObject, SKPhysicsContactDelegate {
+    weak var scene: SKScene?
     
+    var gravity = 1.0
+    
+    init(scene: SKScene) {
+        // need this for the NSObject
+        super.init()
+        
+        self.scene = scene
+    }
+    
+    func testLog() {
+        print("Sent from the SpaceEngine!!")
+    }
 }
